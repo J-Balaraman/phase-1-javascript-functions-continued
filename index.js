013d40289d8dd6b15b2afe9529ef6627afdaac56
jbalaraman <james.balaraman@gmail.com>
2023-06-28 14:03:11 -0600

    That last function really set me off, but I did it

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(place = "go to the office") {
+    return `This Monday, I will ${place}.`;
+}
+
+function wrapAdjective(vis = "*") {
+    return function(kip = "special") {
+        return `You are ${vis}${kip}${vis}!`
+    }
+}
